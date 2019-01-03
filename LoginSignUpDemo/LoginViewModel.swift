2ddaa2cf001ddc9c47542eece64497319bad11a9
changjeffrey829 <changjeffrey@hotmail.com>
2019-01-02 23:50:35 -0500

    second draft

---
+++ LoginSignUpDemo/LoginViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  LoginViewModel.swift
+//  LoginSignUpDemo
+//
+//  Created by Jeffrey Chang on 12/28/18.
+//  Copyright © 2018 Jeffrey Chang. All rights reserved.
+//
+
+import Foundation
+
+
+class LoginViewModel {
+    
+}
